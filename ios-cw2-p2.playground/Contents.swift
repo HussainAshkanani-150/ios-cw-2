f6327a1460122739f64b63e7d43b2cb5eb42f970
hussainashkanani-150 <hashkanani02@gmail.com>
2020-06-17 20:36:57 +0300

    done ✅
    done ✅

---
+++ ios-cw2-p2.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+var fruits = ["mango", "orange", "potato", "peach"]
+print ( fruits[0])
+print (fruits)
+print ( fruits[3])
+print (fruits)
+fruits.append ("strawberry")
+print (fruits)
+fruits.remove(at: 2)
+print (fruits)
+fruits += ["Bana", "watermelon", "pineapple","kiwi"]
+print (fruits)
+fruits.removeAll()
+print (fruits)
